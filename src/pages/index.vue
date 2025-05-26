419387fd5a084dadcb7faf04312c3b73dbaeda44
YudiHardiyanta <yudihardiyanta@gmail.com>
2025-05-26 11:57:37 +0800

    menambahkan index kegiatan

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,8 +1,18 @@
 <template>
-  <Tabs/>
-  <HelloWorld />
+  <div>
+    <AppMenuBar />
+    <v-container>
+      <v-row no-gutters>
+        <v-col cols="12" sm="6">
+          <v-card append-icon="mdi-open-in-new" href="/pendataan?id=2323" prepend-icon="mdi-note-edit" rel="noopener"
+            subtitle="Periode Pendataan 1 Juni - 30 Juni 2025" title="Pendataan Budaya" style="width: 100%;"
+            color="indigo-darken-3"></v-card>
+        </v-col>
+      </v-row>
+    </v-container>
+  </div>
 </template>
 
 <script setup>
-  //
+//
 </script>
